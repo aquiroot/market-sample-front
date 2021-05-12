28669f82f3b1d278bfcebec5de3627ee827a233b
aquiroot <dcolaneri@gmail.com>
2021-05-12 20:36:00 -0300

    [ADD] home page

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import pizza from '../../img/Pizza.svg';
 
 export const Home = () => {
 	return (
-		<>
-			<h1>Home</h1>
-		</>
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<img src={pizza} style={{ width: 550 }} alt="pizza logo" />
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+					alignItems: 'center',
+				}}
+			>
+				<h1>Bienvenidos a Pizza Conmigo</h1>
+				<button className="btn btn-danger m-2">
+					<Link
+						style={{ color: 'white', textDecoration: 'none' }}
+						to="products"
+					>
+						Ver productos
+					</Link>
+				</button>
+			</div>
+		</div>
 	);
 };
